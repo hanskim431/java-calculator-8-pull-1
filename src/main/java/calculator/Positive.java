725ec561024340d39376df9ca42d8ac18ced00fe
fogy431 <fogy431@gmail.com>
2025-10-19 22:28:32 +0900

    fix(positive): null 값 검증 로직 추가

--- src/main/java/calculator/Positive.java
+++ src/main/java/calculator/Positive.java
@@ -4,9 +4,14 @@ public class Positive {
     private final Long number;
 
     public Positive(Long number) {
+        if (number == null) {
+            throw new IllegalArgumentException("양수는 null이 될 수 없습니다.");
+        }
+
         if (number < 0) {
             throw new IllegalArgumentException("양수가 아닌 값이 포함되어 있습니다: " + number);
         }
+
         this.number = number;
     }
 
